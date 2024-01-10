ac6b15ab8a457d63ca88e21b947ddbacf2dfb243
«Roman <research777@mail.ru>
2024-01-10 18:55:58 +0300

    JavaJunSem2

---
+++ src/main/java/example/org/task1/Person.java
@@ -0,0 +1,17 @@
+package example.org.task1;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person() {
+        name = "Name";
+        age = 25;
+    }
+
+    public void displayInfo(){
+        System.out.printf("Name: %s; Age: %d\n%n", name, age);
+    }
+
+}
